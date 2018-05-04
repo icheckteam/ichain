a5deb1d33ca279762530b9e2bfcb92fdce779ba3
thangn <thangn.1411@gmail.com>
2018-05-04 08:09:57 +0700

    update readme

---
+++ x/actor/msgs.go
@@ -0,0 +1,23 @@
+package actor
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// MsgType name to idetify transaction types
+const MsgType = "actor"
+
+// MsgClaim ..
+type MsgClaim struct {
+	Claim
+}
+
+// NOLINT
+func (msg MsgClaim) Type() string                            { return MsgType } //TODO update "stake/declarecandidacy"
+func (msg MsgClaim) Get(key interface{}) (value interface{}) { return nil }
+func (msg MsgClaim) GetSigners() []sdk.Address               { return []sdk.Address{msg.Metadata.Issuer} }
+func (msg MsgClaim) String() string {
+	return fmt.Sprintf("MsgClaim{Issuer: %v}", msg.Metadata.Issuer) // XXX fix
+}
---
+++ x/actor/types.go
@@ -0,0 +1,24 @@
+package actor
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Claim struct
+type Claim struct {
+	ID       string                 `json:"id"`
+	Context  string                 `json:"context"`
+	Content  map[string]interface{} `json:"content"`
+	Metadata Metadata               `json:"metadata"`
+}
+
+// Metadata the claim metadata
+type Metadata struct {
+	CreateTime     time.Time   `json:"create_time"`
+	Issuer         sdk.Address `json:"issuer"`
+	Recipient      sdk.Address `json:"recipient"`
+	ExpirationTime time.Time   `json:"expiration_time"`
+	Revocation     string      `json:"revocation"`
+}
