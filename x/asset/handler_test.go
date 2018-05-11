8f9e8bb12750b388289ec08756be7dcc2f9dd4ed
thangn <thangn.1411@gmail.com>
2018-05-11 08:18:36 +0700

    update circleci test

--- x/asset/handler_test.go
+++ x/asset/handler_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRegister(t *testing.T) {
+func TestHandleRegister(t *testing.T) {
 	ctx, _, keeper := createTestInput(t, false, 1000)
 	addr := addrs[0]
 
