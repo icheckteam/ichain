0d71716beeae412daf51f000f96fa63072086d1b
Vu Duc Dung <ducdung8491@gmail.com>
2018-05-24 08:42:40 +0700

    Add type and register concrete

---
+++ x/invoice/wire.go
@@ -0,0 +1,10 @@
+package invoice
+
+import (
+	"github.com/cosmos/cosmos-sdk/wire"
+)
+
+// Register concrete types on wire codec
+func RegisterWire(cdc *wire.Codec) {
+	cdc.RegisterConcrete(MsgCreate{}, "invoice/MsgCreate", nil)
+}
