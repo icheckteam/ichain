7e4397cfc3f6d2baa9d7f3a4dcc873603a3eaf94
Vu Duc Dung <ducdung8491@gmail.com>
2018-05-24 08:42:50 +0700

    Add mockup test

---
+++ x/invoice/mockup.go
@@ -0,0 +1,122 @@
+package invoice
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/icheckteam/ichain/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tmlibs/log"
+
+	abci "github.com/tendermint/abci/types"
+	crypto "github.com/tendermint/go-crypto"
+	dbm "github.com/tendermint/tmlibs/db"
+
+	"github.com/cosmos/cosmos-sdk/store"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/wire"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/icheckteam/ichain/x/bank"
+)
+
+// dummy addresses used for testing
+var (
+	addrs = []sdk.Address{
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6160"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6161"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6162"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6163"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6164"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6165"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6166"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6167"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6168"),
+		testAddr("A58856F0FD53BF058B4909A21AEC019107BA6169"),
+	}
+
+	// dummy pubkeys used for testing
+	pks = []crypto.PubKey{
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB50"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB51"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB52"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB53"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB54"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB55"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB56"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB57"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB58"),
+		newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB59"),
+	}
+
+	items = []Item{Item{"jav", 1}}
+
+	emptyAddr   sdk.Address
+	emptyPubkey crypto.PubKey
+)
+
+// XXX reference the common declaration of this function
+func subspace(prefix []byte) (start, end []byte) {
+	end = make([]byte, len(prefix))
+	copy(end, prefix)
+	end[len(end)-1]++
+	return prefix, end
+}
+
+func makeTestCodec() *wire.Codec {
+	var cdc = wire.NewCodec()
+
+	// Register Msgs
+	cdc.RegisterInterface((*sdk.Msg)(nil), nil)
+	bank.RegisterWire(cdc)
+	RegisterWire(cdc)
+
+	// Register AppAccount
+	cdc.RegisterInterface((*sdk.Account)(nil), nil)
+	cdc.RegisterConcrete(&types.AppAccount{}, "test/asset/Account", nil)
+	wire.RegisterCrypto(cdc)
+
+	return cdc
+}
+
+// hogpodge of all sorts of input required for testing
+func createTestInput(t *testing.T, isCheckTx bool, initCoins int64) (sdk.Context, InvoiceKeeper) {
+	db := dbm.NewMemDB()
+	keyStake := sdk.NewKVStoreKey("shipping")
+	keyMain := keyStake //sdk.NewKVStoreKey("main") //TODO fix multistore
+
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(keyStake, sdk.StoreTypeIAVL, db)
+	err := ms.LoadLatestVersion()
+	require.Nil(t, err)
+
+	ctx := sdk.NewContext(ms, abci.Header{ChainID: "foochainid"}, isCheckTx, nil, log.NewNopLogger())
+	cdc := makeTestCodec()
+	accountMapper := auth.NewAccountMapper(
+		cdc,                 // amino codec
+		keyMain,             // target store
+		&types.AppAccount{}, // prototype
+	)
+	bank := bank.NewKeeper(accountMapper)
+	shippingKeeper := NewInvoiceKeeper(keyStake, cdc, bank)
+	return ctx, shippingKeeper
+}
+
+func newPubKey(pk string) (res crypto.PubKey) {
+	pkBytes, err := hex.DecodeString(pk)
+	if err != nil {
+		panic(err)
+	}
+	//res, err = crypto.PubKeyFromBytes(pkBytes)
+	var pkEd crypto.PubKeyEd25519
+	copy(pkEd[:], pkBytes[:])
+	return pkEd
+}
+
+// for incode address generation
+func testAddr(addr string) sdk.Address {
+	res, err := sdk.GetAddress(addr)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
