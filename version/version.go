8beead094d1e57729dc6b9f107ca7e19072e550f
thangn <thangn.1411@gmail.com>
2018-06-08 11:36:38 +0700

    update version

--- version/version.go
+++ version/version.go
@@ -2,10 +2,10 @@
 package version
 
 const Maj = "0"
-const Min = "0"
+const Min = "1"
 const Fix = "2"
 
-const Version = "0.0.2"
+const Version = "0.1.2"
 
 // GitCommit set by build flags
 var GitCommit = ""
