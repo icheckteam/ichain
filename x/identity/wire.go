9c7ab7c785ad6b5eb4953fb3e91c58cf14c7412f
thangn <thangn.1411@gmail.com>
2018-06-22 15:22:33 +0700

    update identity module

--- x/identity/wire.go
+++ x/identity/wire.go
@@ -6,7 +6,7 @@ import (
 
 // Register concrete types on wire codec
 func RegisterWire(cdc *wire.Codec) {
-	cdc.RegisterConcrete(MsgCreateClaim{}, "ichain/MsgCreateClaim", nil)
-	cdc.RegisterConcrete(MsgRevokeClaim{}, "ichain/MsgRevokeClaim", nil)
-
+	cdc.RegisterConcrete(MsgCreateClaim{}, "identity/CreateClaim", nil)
+	cdc.RegisterConcrete(MsgRevokeClaim{}, "identity/RevokeClaim", nil)
+	cdc.RegisterConcrete(MsgRevokeClaim{}, "identity/AnswerClaim", nil)
 }
