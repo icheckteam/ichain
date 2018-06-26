c3cc6afdeaececcc53ea4921ddaa655c3cc975e8
thangn <thangn.1411@gmail.com>
2018-06-26 11:14:26 +0700

    fix rest server

--- x/asset/client/rest/root.go
+++ x/asset/client/rest/root.go
@@ -11,7 +11,7 @@ import (
 func RegisterRoutes(ctx context.CoreContext, r *mux.Router, cdc *wire.Codec, kb keys.Keybase, storeName string) {
 	r.HandleFunc("/assets", CreateAssetHandlerFn(ctx, cdc, kb)).Methods("POST")
 	r.HandleFunc("/assets/{id}", QueryAssetRequestHandlerFn(ctx, storeName, cdc)).Methods("GET")
-	r.HandleFunc("/assets/{id}/children", QueryAssetChildrensHandlerFn(ctx, storeName, cdc, kb)).Methods("POST")
+	r.HandleFunc("/assets/{id}/children", QueryAssetChildrensHandlerFn(ctx, storeName, cdc, kb)).Methods("GET")
 	r.HandleFunc("/assets/{id}/add", AddAssetQuantityHandlerFn(ctx, cdc, kb)).Methods("POST")
 	r.HandleFunc("/assets/{id}/subtract", SubtractQuantityBodyHandlerFn(ctx, cdc, kb)).Methods("POST")
 	r.HandleFunc("/assets/{id}/properties", UpdateAttributeHandlerFn(ctx, cdc, kb)).Methods("POST")
