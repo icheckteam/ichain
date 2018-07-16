f7469b67920d3437552eea0ecfe4c5cdd3401572
thangn <thangn.1411@gmail.com>
2018-07-16 08:39:43 +0700

    change shipping wire prefix

--- x/shipping/wire.go
+++ x/shipping/wire.go
@@ -8,10 +8,10 @@ var msgCdc = wire.NewCodec()
 
 // RegisterWire registers concrete types on wire codec
 func RegisterWire(cdc *wire.Codec) {
-	cdc.RegisterConcrete(CreateOrderMsg{}, "ichain/CreateOrderMsg", nil)
-	cdc.RegisterConcrete(ConfirmOrderMsg{}, "ichain/ConfirmOrderMsg", nil)
-	cdc.RegisterConcrete(CompleteOrderMsg{}, "ichain/CompleteOrderMsg", nil)
-	cdc.RegisterConcrete(CancelOrderMsg{}, "ichain/CancelOrderMsg", nil)
+	cdc.RegisterConcrete(CreateOrderMsg{}, "shipping/CreateOrderMsg", nil)
+	cdc.RegisterConcrete(ConfirmOrderMsg{}, "shipping/ConfirmOrderMsg", nil)
+	cdc.RegisterConcrete(CompleteOrderMsg{}, "shipping/CompleteOrderMsg", nil)
+	cdc.RegisterConcrete(CancelOrderMsg{}, "shipping/CancelOrderMsg", nil)
 }
 
 func init() {
